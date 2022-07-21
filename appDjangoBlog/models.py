37c6ed2b3c08ea28de9c3b08da86f2429e4ac882
David Buján Carballal <david.bujan@deusto.es>
2022-07-21 15:12:16 +0200

    DEFAULT_AUTO_FIELD

--- appDjangoBlog/models.py
+++ appDjangoBlog/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 # Create your models here.
 class Post(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=40)
     date = models.DateTimeField(default=datetime.now, blank=True)
     body = models.TextField()
