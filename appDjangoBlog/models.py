4569eeb55c82cae19e7120742a8889add22ddccf
dbujan <david.bujan@deusto.es>
2025-11-27 15:03:13 +0100

    static

--- appDjangoBlog/models.py
+++ appDjangoBlog/models.py
@@ -7,6 +7,6 @@ class Post(models.Model):
     title = models.CharField(max_length=40)
     date = models.DateTimeField(default=datetime.now, blank=True)
     body = models.TextField()
-    image = models.ImageField(upload_to='img',blank=True,null=True,verbose_name='Image')
+    image = models.ImageField(upload_to='media',blank=True,null=True,verbose_name='Image')
     def __str__(self):
         return self.title
--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -124,10 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# MEDIA_URL = '/media/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    BASE_DIR / "img",
+    os.path.join(BASE_DIR, 'media'),
 ]
